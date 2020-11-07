db8c4b1f903559689020689228e92f2b57b12218
JayC1208 <43596559+JayC1208@users.noreply.github.com>
2020-11-07 13:27:27 +0900

    userMotion

--- I3E2/Assets/Scripts/UserMotion.cs
+++ I3E2/Assets/Scripts/UserMotion.cs
@@ -2,28 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UserMotionl : MonoBehaviour
+public class UserMotion : MonoBehaviour
 {
+	public GameObject leftArm;
+	public GameObject rightArm;
+	public GameObject leftLeg;
+	public GameObject rightLeg;
+
+	public SpriteRenderer leftArmRend;
+	public SpriteRenderer rightArmRend;
+	public SpriteRenderer leftLegRend;
+	public SpriteRenderer rightLegRend;
+
+	public Sprite[] LeftArmSprite = new Sprite[3];
+	public Sprite[] rightArmSprite = new Sprite[3];
+	public Sprite[] leftLegSprite = new Sprite[3];
+	public Sprite[] rightLegSprite = new Sprite[3];
+
+	public int leftArmIdx;
+	public int rightArmIdx;
+	public int leftLegIdx;
+	public int rightLegIdx;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+    	leftArmIdx = 0;
+    	rightArmIdx = 0;
+    	leftLegIdx = 0;
+    	rightArmIdx = 0;
+
+    	leftArmRend = leftArm.GetComponent<SpriteRenderer>();
+    	rightArmRend = rightArm.GetComponent<SpriteRenderer>();
+    	leftLegRend = leftLeg.GetComponent<SpriteRenderer>();
+    	rightLegRend = rightLeg.GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)){   //왼팔
-
+        if(Input.GetKeyDown(KeyCode.A)){   //왼팔
+        	leftArmRend.sprite = LeftArmSprite[(leftArmIdx++)%3];
         }
-        if(Input.GetKey(KeyCode.D)){  //오른팔
-        	
+        if(Input.GetKeyDown(KeyCode.D)){  //오른팔
+        	rightArmRend.sprite = rightArmSprite[(rightArmIdx++)%3];
         }
-        if(Input.GetKey(KeyCode.LeftArrow)){  //왼다리
-        	
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){  //왼다리
+        	leftLegRend.sprite = leftLegSprite[(leftLegIdx++)%3];        	
         }
-        if(Input.GetKey(KeyCode.RightArrow)){  //오른다리
-        	
+        if(Input.GetKeyDown(KeyCode.RightArrow)){  //오른다리
+        	rightLegRend.sprite = rightLegSprite[(rightLegIdx++)%3];
         }
     }
 }
