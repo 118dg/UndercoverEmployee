600d77c50bedc0d32359814e7be23f40b30e756e
JayC1208 <43596559+JayC1208@users.noreply.github.com>
2020-11-07 12:07:37 +0900

    initial scene

---
+++ I3E2/Assets/Scripts/TextBlink.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextBlink : MonoBehaviour
+{
+	float time;
+
+	private AudioSource soundTrack;
+	public AudioClip startSound;
+
+	private AudioSource btnClick;
+	public AudioClip btnSound;
+
+	void Start(){
+		this.soundTrack = this.gameObject.AddComponent<AudioSource>();
+		this.soundTrack.clip = this.startSound;
+		this.soundTrack.loop = true;
+		this.soundTrack.Play();
+
+		this.btnClick = this.gameObject.AddComponent<AudioSource>();
+		this.btnClick.clip = this.btnSound;
+		this.btnClick.loop = false;
+	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    	if(Input.GetKeyDown(KeyCode.Space)){
+    		StartCoroutine("changeScene");
+    	}
+
+    	if(time <0.5f){
+    		GetComponent <SpriteRenderer>().color = new Color(1,1,1,1-time);
+    	}else{
+    		GetComponent<SpriteRenderer>().color = new Color(1,1,1,time);
+    		if(time >1f){
+    			time = 0;
+    		}
+    	}
+    	time += Time.deltaTime;
+    }
+
+    IEnumerator changeScene(){
+    	btnClick.Play();
+    	while(true){
+    		yield return new WaitForSeconds(1.0f);
+    		if(!btnClick.isPlaying){
+    			break;
+    		}
+    	}
+		SceneManager.LoadScene("MainScene");
+    }
+}
---
+++ I3E2/Assets/Scripts/UserMotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserMotionl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A)){   //왼팔
+
+        }
+        if(Input.GetKey(KeyCode.D)){  //오른팔
+        	
+        }
+        if(Input.GetKey(KeyCode.LeftArrow)){  //왼다리
+        	
+        }
+        if(Input.GetKey(KeyCode.RightArrow)){  //오른다리
+        	
+        }
+    }
+}
