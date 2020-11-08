eef5c602bfb435cc3b4696eab9d5ae1f9919a2fb
JayC1208 <43596559+JayC1208@users.noreply.github.com>
2020-11-08 14:30:19 +0900

    final ver2

--- I3E2/Assets/Scripts/TextBlink.cs
+++ I3E2/Assets/Scripts/TextBlink.cs
@@ -13,7 +13,14 @@ public class TextBlink : MonoBehaviour
 	private AudioSource btnClick;
 	public AudioClip btnSound;
 
+	public GameObject guide;
+	private AudioSource effect;
+	public AudioClip guideSound;
+
 	void Start(){
+
+		guide.SetActive(false);
+		
 		this.soundTrack = this.gameObject.AddComponent<AudioSource>();
 		this.soundTrack.clip = this.startSound;
 		this.soundTrack.loop = true;
@@ -22,24 +29,40 @@ public class TextBlink : MonoBehaviour
 		this.btnClick = this.gameObject.AddComponent<AudioSource>();
 		this.btnClick.clip = this.btnSound;
 		this.btnClick.loop = false;
+
+		this.effect = this.gameObject.AddComponent<AudioSource>();
+		this.effect.clip = this.guideSound;
+		this.effect.loop = false;
 	}
     // Update is called once per frame
     void Update()
     {
+		if(Input.GetKeyDown(KeyCode.Tab)){
+			if(!guide.active){
+				effect.Play();
+				guide.SetActive(true);
+			}else{
+				guide.SetActive(true);
+			}
 
-    	if(Input.GetKeyDown(KeyCode.Space)){
-    		StartCoroutine("changeScene");
+    	}else if(Input.GetKeyUp(KeyCode.Tab)){
+			guide.SetActive(false);
     	}
+    	else{
+    		if(Input.GetKeyDown(KeyCode.Space)){
+	    		StartCoroutine("changeScene");
+	    	}
 
-    	if(time <0.5f){
-    		GetComponent <SpriteRenderer>().color = new Color(1,1,1,1-time);
-    	}else{
-    		GetComponent<SpriteRenderer>().color = new Color(1,1,1,time);
-    		if(time >1f){
-    			time = 0;
-    		}
+	    	if(time <0.5f){
+	    		GetComponent <SpriteRenderer>().color = new Color(1,1,1,1-time);
+	    	}else{
+	    		GetComponent<SpriteRenderer>().color = new Color(1,1,1,time);
+	    		if(time >1f){
+	    			time = 0;
+	    		}
+	    	}
+	    	time += Time.deltaTime;
     	}
-    	time += Time.deltaTime;
     }
 
     IEnumerator changeScene(){
